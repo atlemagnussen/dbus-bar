a19a17e70a00ae9cb9cd1e626805029940fdc744
Atle Magnussen <atlemagnussen@gmail.com>
2019-06-02 16:37:12 +0200

    got ip address from nm

--- python/networkmanager.py
+++ python/networkmanager.py
@@ -19,8 +19,10 @@ def test():
     try:
         act_con_name = get_active_connection_name()
         # print(act_con_name)
-        con = get_connection(act_con_name)
-        print(con)
+        ip4conf = get_connection(act_con_name)
+        print(ip4conf)
+        ip4add = get_ip_address(ip4conf)
+        print(ip4add)
     except Exception as ex:
         print('Network manager not active probably')
 
@@ -42,5 +44,17 @@ def get_connection(path):
     #    print(f'{prop}={val}')
     spec = props.get("Ip4Config")
     return str(spec)
+
+def get_ip_address(path):
+    """get address"""
+    bus_obj = get_bus_object("org.freedesktop.NetworkManager", path)
+    props_iface = dbus.Interface(bus_obj, dbus_interface='org.freedesktop.DBus.Properties')
+    props = props_iface.GetAll("org.freedesktop.NetworkManager.IP4Config")
+    #for prop in props:
+    #    val = str(props.get(prop))
+    #    print(f'{prop}={val}')
+    address = props.get("AddressData")
+    return str(address[0].get('address'))
+
 if __name__ == "__main__":
     test()
