18c586b480fdab9b435b114cc328d7543bb8b55f
Atle Magnussen <atlemagnussen@gmail.com>
2019-06-02 16:19:15 +0200

    added more stuff in network manager

--- python/networkmanager.py
+++ python/networkmanager.py
@@ -17,16 +17,30 @@ def get_interface(bus_obj, interfacename):
 def test():
     """test"""
     try:
-        bus_obj = get_bus_object("org.freedesktop.NetworkManager", "/org/freedesktop/NetworkManager")
-        props_iface = dbus.Interface(bus_obj, dbus_interface='org.freedesktop.DBus.Properties')
-        props = props_iface.GetAll("org.freedesktop.NetworkManager")
-        active = props.get('ActiveConnections')
-        print(active[0])
-        # interface = get_interface(bus_obj, INTERFACENAME)
-        # interface.PlayPause()
+        act_con_name = get_active_connection_name()
+        # print(act_con_name)
+        con = get_connection(act_con_name)
+        print(con)
     except Exception as ex:
         print('Network manager not active probably')
 
+def get_active_connection_name():
+    """active connections names"""
+    bus_obj = get_bus_object("org.freedesktop.NetworkManager", "/org/freedesktop/NetworkManager")
+    props_iface = dbus.Interface(bus_obj, dbus_interface='org.freedesktop.DBus.Properties')
+    props = props_iface.GetAll("org.freedesktop.NetworkManager")
+    active = props.get('ActiveConnections')
+    return str(active[0])
 
+def get_connection(path):
+    """get connection"""
+    bus_obj = get_bus_object("org.freedesktop.NetworkManager", path)
+    props_iface = dbus.Interface(bus_obj, dbus_interface='org.freedesktop.DBus.Properties')
+    props = props_iface.GetAll("org.freedesktop.NetworkManager.Connection.Active")
+    # for prop in props:
+    #    val = str(props.get(prop))
+    #    print(f'{prop}={val}')
+    spec = props.get("Ip4Config")
+    return str(spec)
 if __name__ == "__main__":
     test()
