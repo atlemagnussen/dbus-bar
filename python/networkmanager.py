da94656f6442e602b56a3c72e9080d1885793040
Atle Magnussen <atlemagnussen@gmail.com>
2019-06-02 17:36:02 +0200

    added more shit

--- python/networkmanager.py
+++ python/networkmanager.py
@@ -3,7 +3,34 @@
 # -*- coding: utf-8 -*-
 # from pydbus import SystemBus
 import dbus
-# from gi.repository import GLib
+from dbus.mainloop.glib import DBusGMainLoop
+from gi.repository import GLib
+
+def init():
+    """init"""
+    loop = GLib.MainLoop()
+    try:
+        loop.run()
+    except KeyboardInterrupt:
+        loop.quit()
+    try:
+        DBusGMainLoop(set_as_default=True)
+        bus = dbus.SystemBus()
+        bus_obj = get_bus_object("org.freedesktop.NetworkManager", "/org/freedesktop/NetworkManager")
+        interface = dbus.Interface(bus_obj, dbus_interface="org.freedesktop.NetworkManager")
+        # bus.add_signal_receiver(sig_handler, "StateChanged", dbus_interface=interface)
+        interface.connect_to_signal(None, sig_handler, sender_keyword='sender')
+        # bus_obj.ListenForSignal("org.freedesktop.NetworkManager.StateChanged", dbus.Array(signature='o'), dbus_interface="org.freedesktop.NetworkManager")
+        # bus_obj.add_signal_receiver(sig_handler, "StateChanged")
+
+    except Exception as ex:
+        print('Network manager not active probably')
+    # pulse_core = bus_obj.get_object(object_path='/org/pulseaudio/core1')
+    # pulse_core.ListenForSignal(MAIN + '.' + STATSIG, dbus.Array(signature='o'), dbus_interface=IFACE)
+
+def sig_handler(sender=None):
+    """handler"""
+    print(sender)
 
 def get_bus_object(name, path):
     """bus object"""
@@ -50,11 +77,11 @@ def get_ip_address(path):
     bus_obj = get_bus_object("org.freedesktop.NetworkManager", path)
     props_iface = dbus.Interface(bus_obj, dbus_interface='org.freedesktop.DBus.Properties')
     props = props_iface.GetAll("org.freedesktop.NetworkManager.IP4Config")
-    #for prop in props:
-    #    val = str(props.get(prop))
-    #    print(f'{prop}={val}')
+    for prop in props:
+        val = str(props.get(prop))
+        print(f'{prop}={val}')
     address = props.get("AddressData")
     return str(address[0].get('address'))
 
 if __name__ == "__main__":
-    test()
+    init()
--- python/pulse.py
+++ python/pulse.py
@@ -60,7 +60,7 @@ def init():
     pulse_core.ListenForSignal(MAIN + '.' + STATSIG, dbus.Array(signature='o'), dbus_interface=IFACE)
     pulse_core.ListenForSignal(MAIN + '.' + VOLSIG, dbus.Array(signature='o'), dbus_interface=IFACE)
     pulse_core.ListenForSignal(MAIN + '.' + MUTESIG, dbus.Array(signature='o'), dbus_interface=IFACE)
-    interface = dbus.Interface(pulse_bus, dbus_interface='org.PulseAudio.Core1')
+    # interface = dbus.Interface(pulse_bus, dbus_interface='org.PulseAudio.Core1')
 
     loop = GLib.MainLoop()
 
